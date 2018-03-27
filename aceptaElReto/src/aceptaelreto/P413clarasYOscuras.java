590b0c3fae1c9a03ae02f04fc5a034f1b5851f66
Wally <Wally@DESKTOP-KDFHPPV>
2018-03-27 15:18:47 +0200

    Aceptado, 0.701 segs.

--- aceptaElReto/src/aceptaelreto/P413clarasYOscuras.java
+++ aceptaElReto/src/aceptaelreto/P413clarasYOscuras.java
@@ -1,26 +1,33 @@
 package aceptaelreto;
 
-import java.util.Random;
 import java.util.Scanner;
 
 /**
  *
- * @author Wally
+ * @author Wallington Bandeira
+ *
+ * Tiempo de ejecucion esperado: ~0.700 segs.
+ *
+ * Memoria: 1729 KiB
+ *
+ * Puesto 177 (en el momento de hacer el envío)
  */
 public class P413clarasYOscuras {
 
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
-        double x, y, multi, divi, cantidad = lector.nextInt();
+        int x, y, cantidad;
+        double multi, divi;
+        cantidad = lector.nextInt();
         for (int i = 0; i < cantidad; i++) {
             x = lector.nextInt();
             y = lector.nextInt();
             multi = x * y;
             divi = multi / 2;
-            if (divi % 2 != 0) {
-                System.out.println(divi + 1 +" "+ divi);
+            if (divi > (int) divi) {
+                System.out.println((int) divi + 1 + " " + (int) divi);
             } else {
-                System.out.println(divi + " " + divi);
+                System.out.println((int) divi + " " + (int) divi);
             }
         }
     }
