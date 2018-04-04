1ce3ace5ce0edf0ec19b7d5b9308e1b240019658
Wally <Wally@DESKTOP-KDFHPPV>
2018-04-04 23:55:35 +0200

    Aceptado, 1.269 segs

--- aceptaElReto/src/aceptaelreto/P151esMatrizIdentidad.java
+++ aceptaElReto/src/aceptaelreto/P151esMatrizIdentidad.java
@@ -4,11 +4,11 @@ package aceptaelreto;
  *
  * @author Wallington Bandeira
  *
- * Tiempo de ejecucion esperado:
+ * Tiempo de ejecucion esperado: ~1.269 segs.
  *
- * Memoria:
+ * Memoria: 1747 KiB
  *
- * Puesto (en el momento de hacer el envío)
+ * Puesto 495 (en el momento de hacer el envío)
  */
 import java.util.Scanner;
 
@@ -21,55 +21,25 @@ public class P151esMatrizIdentidad {
 
         do {
             veri = true;
-            tamano = lector.nextByte();
-            for (int i = 0; i < tamano && tamano != 0; i++) {
-                for (int j = 0; j < tamano; j++) {
-                    numero = lector.nextByte();
-                    if (i == j && numero != 1) {
-                        veri = false;
-                    } else if (i != j && numero != 0) {
-                        veri = false;
+            tamano = lector.nextInt();
+            if (tamano != 0) {
+                for (int i = 0; i < tamano; i++) {
+                    for (int j = 0; j < tamano; j++) {
+                        numero = lector.nextInt();
+                        if (i == j && numero != 1) {
+                            veri = false;
+                        } else if (i != j && numero != 0) {
+                            veri = false;
+                        }
                     }
                 }
-            }
 
-            if (tamano != 0 && veri) {
-                System.out.println("SI");
-            } else if (tamano != 0 && !veri) {
-                System.out.println("NO");
+                if (veri) {
+                    System.out.println("SI");
+                } else if (!veri) {
+                    System.out.println("NO");
+                }
             }
         } while (tamano != 0);
-
-        /*
-        do {
-            veri = true;
-            tamano = lector.nextByte();
-            //introduce los datos en la array
-            for (int i = 0; i < tamano && tamano != 0; i++) {
-                for (int j = 0; j < tamano; j++) {
-                    if (i == j) {
-                        raiz[i][j] = 1;
-                    } else {
-                        raiz[i][j] = 0;
-                    }
-                }
-            }
-            //verifica los datos de la array
-            for (int i = 0; i < tamano && veri; i++) {
-                for (int j = 0; j < tamano && veri; j++) {
-                    if (i == j && raiz[i][j] != 1) {
-                        veri = false;
-                    } else if (i != j && raiz[i][j] != 0) {
-                        veri = false;
-                    }
-                }
-            }
-            // verifica qué debe imprimir
-            if (tamano != 0 && veri) {
-            System.out.println("SI");
-            } else if (tamano != 0 && !veri) {
-                System.out.println("NO");
-            }
-        } while (tamano != 0);*/
     }
 }
